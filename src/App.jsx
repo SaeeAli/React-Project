1b062836b4fb862c4c9b5fa46257c430c9b35acb
ali saee <saeeali1979@gmail.com>
2020-06-22 18:28:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Form from './components/Form.jsx';
+import ProfileDetails from './components/ProfileDetails.jsx';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gitun: 'No username',
+      infoclean : '',
+      formData: {
+        username: '',
+      },
+}
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange= this.handleFormChange.bind(this);
+  }
+handleUserFormSubmit(event) {
+    event.preventDefault();
+       axios.get('https://api.github.com/users/'+this.state.formData.username)
+    .then(response => this.setState({
+      gitun: response.data.login,
+      infoclean: response.data,
+    })).catch((err) => { console.log(err); });
+  };
+handleFormChange(event) {
+    const obj = this.state.formData;
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">GitHub User-Info</h1>
+        </header>
+        <p className="App-intro">
+          This is my first React App
+        </p>
+        <hr></hr>
+        <Form
+          formData={this.state.formData}
+          handleUserFormSubmit={this.handleUserFormSubmit}
+          handleFormChange={this.handleFormChange}
+        />
+        <hr></hr>
+        Profile Details:
+        <ProfileDetails infoclean={this.state.infoclean}/>
+</div>
+    );
+  }
+}
+export default App;
